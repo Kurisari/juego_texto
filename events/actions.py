380a1ec173f46dfe17187bab18ef99d8733884d0
Cristian Aragón <crisarag8.sal@gmail.com>
2023-11-26 21:25:39 -0600

    Cambio de nombre del diccionario de acciones

--- events/actions.py
+++ events/actions.py
@@ -1,4 +1,4 @@
-acciones_dict = {
+acciones = {
     'caminar': "Caminas hacia adelante.",
     'irse': "Decides abandonar el lugar.",
     'hablar': "Inicias una conversación con alguien cercano.",
