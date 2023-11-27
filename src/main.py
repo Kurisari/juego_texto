c64536da63f8278eb63f3c5fce27899e83a7c332
Cristian Aragón <crisarag8.sal@gmail.com>
2023-11-26 22:11:21 -0600

    Implementación de escenas

--- src/main.py
+++ src/main.py
@@ -13,13 +13,65 @@ def type_print(texto, velocidad=0.07):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(velocidad)
+        
+class Escena:
+    def __init__(self, nombre, descripcion, acciones):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.acciones = acciones
+
+    def mostrar_descripcion(self):
+        type_print(self.descripcion)
+
+    def ejecutar_accion(self, accion_usuario):
+        if accion_usuario in self.acciones:
+            accion = self.acciones[accion_usuario]
+            if isinstance(accion, dict):
+                type_print(accion.get('mensaje', 'Mensaje no definido para esta acción.'))
+                nueva_escena = accion.get('a_escena')
+                if nueva_escena:
+                    return nueva_escena
+            else:
+                type_print(accion)
+        else:
+            type_print("No entiendo esa acción. Intenta nuevamente.")
 
 class Juego:
     def __init__(self):
-        self.camino = ""
-        self.final = ""
-        self.nombre_jugador = ""
-        self.accion = ""
+        self.escenas = {
+            'inicio': Escena('inicio', "Te encuentras en el laboratorio donde se creó Esditeo.", {
+                'caminar': {"mensaje": "Caminas hacia adelante.", "a_escena": 'bosque'},
+                'irse': "Decides abandonar el laboratorio.",
+                'hablar': "Inicias una conversación con un colega.",
+            }),
+            'bosque': Escena('bosque', "Has llegado a un misterioso bosque.", {
+                'explorar': "Te aventuras más profundamente en el bosque.",
+                'regresar': "Decides volver al laboratorio.",
+                'descansar': "Tomar un descanso en medio del bosque.",
+            }),
+        }
+        self.escena_actual = 'inicio'
+
+    def cambiar_escena(self, nombre_escena):
+        self.escena_actual = nombre_escena
+        self.escenas[nombre_escena].mostrar_descripcion()
+
+    def ejecutar_accion(self, accion_usuario):
+        if accion_usuario == 'salir':
+            type_print("Gracias por jugar. ¡Hasta luego!")
+            sys.exit()
+        acciones_escena_actual = self.escenas[self.escena_actual].acciones
+        if accion_usuario in acciones_escena_actual:
+            accion = acciones_escena_actual[accion_usuario]
+            if isinstance(accion, dict):
+                type_print(accion.get('mensaje', 'Mensaje no definido para esta acción.'))
+                nueva_escena = accion.get('a_escena')
+                if nueva_escena:
+                    self.cambiar_escena(nueva_escena)
+            else:
+                type_print(accion)
+        else:
+            type_print("No entiendo esa acción. Intenta nuevamente.")
 
     def iniciar(self):
         pygame.init()
@@ -48,20 +100,14 @@ solo. Otras entidades, tanto humanas como artificiales, tienen sus propios plane
 
 El juego tiene múltiples caminos y finales, dependiendo de las decisiones que tomes.
 Sé astuto con tus decisiones y mucha suerte, que el poder de la IA esté de tu lado…""")
-        self.ejecutar_accion()
-        
-
-    def ejecutar_accion(self):
+        print()
+        self.escenas[self.escena_actual].mostrar_descripcion()
         while True:
+            print()
             type_print("¿Qué deseas hacer?")
             accion_usuario = input().lower()
-            if accion_usuario == 'salir':
-                type_print("Gracias por jugar. ¡Hasta luego!")
-                sys.exit()
-            if accion_usuario in actions.acciones:
-                type_print(actions.acciones[accion_usuario])
-            else:
-                type_print("No entiendo esa acción. Intenta nuevamente.")
+            self.ejecutar_accion(accion_usuario)
+
 
 juego = Juego()
 juego.iniciar()
